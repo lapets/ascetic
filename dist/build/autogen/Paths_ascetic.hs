b511c8132b31495011c0c71688a8d42f2c544720
lapets <lapets@gmail.com>
2013-07-10 19:31:39 -0400

    Update build scripts and license; rebuild latest version.

--- dist/build/autogen/Paths_ascetic.hs
+++ dist/build/autogen/Paths_ascetic.hs
@@ -14,13 +14,13 @@ catchIO = Exception.catch
 
 
 version :: Version
-version = Version {versionBranch = [0,0,0,1], versionTags = []}
+version = Version {versionBranch = [0,0,0,2], versionTags = []}
 bindir, libdir, datadir, libexecdir :: FilePath
 
-bindir     = "C:\\Users\\al\\AppData\\Roaming\\cabal\\bin"
-libdir     = "C:\\Users\\al\\AppData\\Roaming\\cabal\\ascetic-0.0.0.1\\ghc-7.4.2"
-datadir    = "C:\\Users\\al\\AppData\\Roaming\\cabal\\ascetic-0.0.0.1"
-libexecdir = "C:\\Users\\al\\AppData\\Roaming\\cabal\\ascetic-0.0.0.1"
+bindir     = "C:\\Documents and Settings\\Administrator\\Application Data\\cabal\\bin"
+libdir     = "C:\\Documents and Settings\\Administrator\\Application Data\\cabal\\ascetic-0.0.0.2\\ghc-7.4.2"
+datadir    = "C:\\Documents and Settings\\Administrator\\Application Data\\cabal\\ascetic-0.0.0.2"
+libexecdir = "C:\\Documents and Settings\\Administrator\\Application Data\\cabal\\ascetic-0.0.0.2"
 
 getBinDir, getLibDir, getDataDir, getLibexecDir :: IO FilePath
 getBinDir = catchIO (getEnv "ascetic_bindir") (\_ -> return bindir)
